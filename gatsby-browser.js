884cb503526da451aaa098b8dc5949e6e02c6db3
Liz Baker <lbaker@newrelic.com>
2022-11-22 15:16:47 -0800

    chore: Point to docs dev

--- gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-const onClientEntry = () => {
-  // Expose both globals so that the NR1 docs can read it.
-  window.__webpack_public_path__ = "https://docs.newrelic.com/";
-};
-
-export { onClientEntry };
--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ exports.createPages = ({ actions }) => {
   const { createRedirect } = actions;
   createRedirect({
     fromPath: `/docs/*`,
-    toPath: `https://docs.newrelic.com/docs/*`,
+    toPath: `https://docs-dev.newrelic.com/docs/*`,
     statusCode: 200,
   });
 };
