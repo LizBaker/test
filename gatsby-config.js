0f3ecff9dd98f2f0411de57ed45aacb9574dbd57
Liz Baker <lbaker@newrelic.com>
2022-12-16 12:29:11 -0800

    add header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,12 @@
 module.exports = {
-  assetPrefix: `https://docs-dev.newrelic.com`,
-  plugins: [`gatsby-plugin-gatsby-cloud`],
+  plugins: [
+    {
+      resolve: "gatsby-plugin-gatsby-cloud",
+      options: {
+        headers: {
+          "/*": ["Referrer-Policy: no-referrer-when-downgrade"],
+        },
+      },
+    },
+  ],
 };
