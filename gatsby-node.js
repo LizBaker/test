8dbb5deca3afcc1d4533532d7628eae6f3a673c7
Liz Baker <lbaker@newrelic.com>
2023-01-10 16:33:41 -0800

    test jp and kr sites

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,13 @@
 exports.createPages = ({ actions }) => {
   const { createRedirect } = actions;
   createRedirect({
-    fromPath: `/docs/*`,
-    toPath: `https://docs-dev.newrelic.com/docs/*`,
+    fromPath: `kr/*`,
+    toPath: `https://docswebsitekr.gatsbyjs.io/kr/*`,
+    statusCode: 200,
+  });
+  createRedirect({
+    fromPath: `jp/*`,
+    toPath: `https://docswebsitejp.gatsbyjs.io/jp/*`,
     statusCode: 200,
   });
 };
